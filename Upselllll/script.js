6e4556855b46faba4553621984e4c8a391196400
Pedro Ramos <opedroramosbr@gmail.com>
2023-03-17 11:24:06 -0300

    000

---
+++ Upselllll/script.js
@@ -0,0 +1,11 @@
+
+setTimeout(
+    function tempo() {
+        var elemento1 = document.querySelector('#botãokiwi')
+
+        elemento1.style.display = "flex";
+
+
+
+    },)
+    /*tempo em milisegundos*/
